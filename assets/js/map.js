4427e7ab086680b1ea64331e20d46c6ee07febaf
RJHCarto <33897218+RJHCarto@users.noreply.github.com>
2018-03-25 00:50:28 +0000

    MinZoom Set

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,7 @@
   // load the map
     var mymap = L.map('mapid').setView([55.940718, -36.894996], 2);
     // load the tiles
-    L.tileLayer('https://api.mapbox.com/styles/v1/rjhargreaves/cjcv3d22i04bp2rpza7tjx1c4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmpoYXJncmVhdmVzIiwiYSI6ImNpa3JmbDJiazAwMDF3Y20xMHoyaXowdDAifQ.78vWSemMDwn42TwMuxfODw', {maxZoom: 18,attribution: 'Map data &copy; <ahref="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    L.tileLayer('https://api.mapbox.com/styles/v1/rjhargreaves/cjcv3d22i04bp2rpza7tjx1c4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmpoYXJncmVhdmVzIiwiYSI6ImNpa3JmbDJiazAwMDF3Y20xMHoyaXowdDAifQ.78vWSemMDwn42TwMuxfODw', {minZoom:2, maxZoom: 18, attribution: 'Map data &copy; <ahref="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
         '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,' 
       + 
         'Imagery © <a href="http://mapbox.com">Mapbox</a>',
